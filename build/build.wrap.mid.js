f3cdadcd1a689fe1dab2d1647ecf5a9c80665746
Calvin Mann <calvinmann@gmail.com>
2016-10-04 18:39:05 +0200

    Added source style check. Removed build files and switched to browserify. Updated dependencies to latest versions

--- build/build.wrap.mid.js
+++
@@ -1,3 +0,0 @@
-exports = reqCache;
-escodegen = { };
-(function() {
--- gruntfile.js
+++ gruntfile.js
@@ -3,32 +3,26 @@ module.exports = function(grunt) {
         jshint: {
             src: ['src/**/*.js'],
             options: {
+                jshintrc: true,
                 globals: {
                     module: true,
                     require: true
                 }
             }
         },
-        shell: {
-            escodegen: {
-                command: 'node node_modules/commonjs-everywhere/bin/cjsify -a path: build/escodegen.entry.js > build/escodegen.js'
+        jscs: {
+            src: {
+                src: 'src/**/*.js',
+                options: {
+                    config: '.jscsrc'
+                }
             }
         },
-        concat: {
-            options: {
-                seperator: ';\n'
-            },
-            dist: {
-                src: [
-                    'build/build.wrap.start.js',
-                    'node_modules/esprima/esprima.js',
-                    'build/build.wrap.mid.js',
-                    'build/escodegen.js',
-                    'build/build.wrap.mid2.js',
-                    'src/parser.js',
-                    'build/build.wrap.end.js'
-                ],
-                dest: 'dist/conditions.js'
+        browserify: {
+            main: {
+                files: {
+                    'dist/conditions.js': ['src/index.js']
+                }
             }
         },
         uglify: {
@@ -40,17 +34,17 @@ module.exports = function(grunt) {
                     'dist/conditions.min.js': ['dist/conditions.js']
                 }
             }
-
         }
     });
 
-    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jscs');
 
-    //Create the tasks
-    grunt.registerTask('build', ['shell:escodegen', 'concat:dist', 'uglify:dist']);
-    grunt.registerTask('test', ['jshint:src']);
+    // Create the tasks
+    grunt.registerTask('build', ['browserify:main', 'uglify:dist']);
+    grunt.registerTask('test', ['jshint:src', 'jscs:src']);
     grunt.registerTask('default', ['test', 'build']);
 };
